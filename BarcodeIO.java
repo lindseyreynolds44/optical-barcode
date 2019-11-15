51b20934c11e21f1aee84fb236c4e39799a5d4c4
Lindsey Reynolds <lreynolds@csumb.edu>
2019-11-14 18:41:39 -0800

    copied barcodeIO

---
+++ BarcodeIO.java
@@ -0,0 +1,33 @@
+/**
+ * Phase 1
+ * 
+ * BarcodeIO.java
+ * 
+ * @author Ricardo Barbosa 
+ * @version November 14, 2019
+ * 
+ * Outlines the operations for BarcodeImages
+ * 
+ */
+
+public interface BarcodeIO
+{
+    //stores a copy of the image 
+    public boolean scna(BarcodeImage bc);
+
+    //stores a string to be encoded into BarcodeImage
+    public boolean readText(String text);
+
+    //analyzes internal text to be encoded into BarcodeImage
+    public boolean generateImageFromText();
+
+    //analyzes internal BarcodeImage to decode to text 
+    public boolean translateImageToText();
+
+    //prints string to console
+    public void displayTextToConsole();
+
+    //prints dot matrix to the console
+    public void displayImageToConsole();
+    
+}
