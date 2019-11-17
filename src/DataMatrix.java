fda26642e492ee71e386174d2dd3087a4c756ce7
barbosaricardo <ric3639@gmail.com>
2019-11-16 23:47:11 -0800

    dan's branch added

--- src/DataMatrix.java
+++ src/DataMatrix.java
@@ -246,63 +246,171 @@ public class DataMatrix implements BarcodeIO
 
    /****************************************END*****OF*******PERSON2************************************/
    
-   // PRIVATE METHODS
-
-   private int computeSignalWidth() 
-   {
-      /* 
-      Assuming that the image is correctly situated in the lower-left corner of the larger boolean array, these methods use the "spine" of the array (left and bottom BLACK) to determine the actual size.
-      */
-   }
-   private int computeSignalHeight() 
-   {
-      /* 
-      Assuming that the image is correctly situated in the lower-left corner of the larger boolean array, these methods use the "spine" of the array (left and bottom BLACK) to determine the actual size.
-      */
-   }
-
-   private void cleanImage() 
-   {
-      /*
-      This private method will make no assumption about the placement of the "signal" within a passed-in BarcodeImage.  In other words, the in-coming BarcodeImage may not be lower-left justified. 
-
-      The cleanImage() method would be called from within scan() and would move the signal to the lower-left of the larger 2D array.  And, since scan() is called by the constructor, that implies that the image gets adjusted upon construction.  This kind of standardization represents the many other image processing tasks that would be implemented in the scan() method.  Error correction would be done at this point in a real class design. 
-      */
-   }
-
-   // Method to help with manipulation in cleanImage()
-   private void moveImageToLowerLeft()
-   {
-
-   } 
-   // Method to help with manipulation in cleanImage()
-   private void shiftImageDown(int offset)
-   {
-
-   }
-   // Method to help with manipulation in cleanImage()
-   private void shiftImageLeft(int offset)
-   {
-
-   }
-
-
-   // Optional 
-   public void displayRawImage() 
-   {
-      /*
-      Can be implemented to show the full image data including the blank top and right.  It is a useful debugging tool.
-      */
-
-   }
-
+  // PRIVATE METHODS
    
-   // Optional 
-   private void clearImage() 
-   {
-      // a nice utility that sets the image to white =  false.
-   }
-
-}
+   /**
+    * Computes the width of the signal assuming it's already been shifted to
+    * the lower left corner
+    * @return The width of the signal.
+    */
+    private int computeSignalWidth() 
+    {
+       /* 
+       Assuming that the image is correctly situated in the lower-left corner of the larger boolean array, these methods use the "spine" of the array (left and bottom BLACK) to determine the actual size.
+       */
+       int counter = 0;
+       for(int col = 0; col < image.MAX_WIDTH; col++)
+       {
+         if(image.getPixel(image.MAX_HEIGHT - 1, col))
+             counter++;
+       }
+       return counter; 
+    }
+    /*
+     * Computes the height of the signal assuming it's already been shifted
+     * to the lower left corner.
+     * @return The height of the signal
+     */
+    private int computeSignalHeight() 
+    {
+       /* 
+       Assuming that the image is correctly situated in the lower-left corner of the larger boolean array, these methods use the "spine" of the array (left and bottom BLACK) to determine the actual size.
+       */
+       int counter = 0;
+       int firstCol = 0;
+       for(int row = 0; row < image.MAX_HEIGHT; row++)
+          if(image.getPixel(row,firstCol))
+            counter++;
+        return counter;  
+    }
+ 
+    private void cleanImage() 
+    {
+       /*
+       This private method will make no assumption about the placement of the "signal" within a passed-in BarcodeImage.  In other words, the in-coming BarcodeImage may not be lower-left justified. 
+ 
+       The cleanImage() method would be called from within scan() and would move the signal to the lower-left of the larger 2D array.  And, since scan() is called by the constructor, that implies that the image gets adjusted upon construction.  This kind of standardization represents the many other image processing tasks that would be implemented in the scan() method.  Error correction would be done at this point in a real class design. 
+        */
+       moveImageToLowerLeft();
+    }
+ 
+    // Method to help with manipulation in cleanImage()
+    private void moveImageToLowerLeft()
+    {
+       int downOffset = countBlankRows();
+       if(downOffset != 0)
+          shiftImageDown(countBlankRows());
+       
+       int leftOffset = countBlankColumns();
+       if(leftOffset != 0)
+          shiftImageLeft(countBlankColumns());
+    } 
+    /**
+     * Counts the number of blank rows from the bottom
+     * @return Int count of blank rows from the bottom.
+     */
+    private int countBlankRows()
+    {
+      boolean blankRow = false;
+      int countRow = 0;
+      for(int row = image.MAX_HEIGHT - 1; row >= 0; row--)
+      {
+        for(int col = 0; col < image.MAX_WIDTH; col++)
+        {
+           if(!image.getPixel(row,col))
+               blankRow = true;
+           if(image.getPixel(row,col))
+           {
+             blankRow = false;
+             return countRow;
+           }            
+        }
+        if(blankRow)
+           countRow++;
+      }   
+      return countRow;
+    }
+    
+    /**
+     * Counts the number of blank columns from the left.
+     * @return Int count of blank columns from the left.
+     */
+    private int countBlankColumns()
+    {
+      boolean blankCol = false;
+      int countCol = 0;
+      for(int col = 0; col < image.MAX_WIDTH; col++)
+      {
+        for(int row = 0; row < image.MAX_HEIGHT; row++)
+        {
+           if(!image.getPixel(row,col))
+               blankCol = true;
+           if(image.getPixel(row,col))
+           {
+             blankCol = false;
+             return countCol;
+           }            
+        }
+        if(blankCol)
+           countCol++;
+      }   
+      return countCol;
+    }
+    
+    /**
+     * Shift the array to the left most column
+     * @param offset
+     */
+    private void shiftImageDown(int offset)
+    {
+       int lastRow = image.MAX_HEIGHT - 1;
+       int shiftBy = lastRow - offset;
+ 
+       for(int row = lastRow; row >= 0; row--)
+       {
+          for(int col = 0; col < image.MAX_WIDTH; col++)
+          {
+             image.setPixel(row, col, image.getPixel(shiftBy,col));
+             image.setPixel(shiftBy,col,false);
+          }
+          shiftBy--;
+          if(shiftBy == 0)
+             break;
+       }
+    }
+    /**
+     * Shifts the Array the bottom most row.
+     * @param offset
+     */
+    private void shiftImageLeft(int offset)
+    {
+       for(int col = 0; col < image.MAX_WIDTH; col++,offset++)
+       {
+          for(int row = 0; row < image.MAX_HEIGHT; row++)
+          {
+             image.setPixel(row,col,image.getPixel(row,offset));
+             image.setPixel(row,offset,false);
+          }
+       }
+    }
+ 
+ 
+    // Optional 
+    public void displayRawImage() 
+    {
+       /*
+       Can be implemented to show the full image data including the blank top and right.  It is a useful debugging tool.
+        */
+ 
+    }
+ 
+ 
+    // Optional 
+    private void clearImage() 
+    {
+       // a nice utility that sets the image to white =  false.
+    }
+ 
+ }
 
 
